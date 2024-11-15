f4fcf1221780a8342c1f89ff5925827b119a2bea
binhnguyendoan <126456308+binhnguyendoan@users.noreply.github.com>
2024-11-15 11:55:19 +0700

    Update template Contact us

--- app/code/Mindarc/Survey/Controller/Index/Index.php
+++ app/code/Mindarc/Survey/Controller/Index/Index.php
@@ -6,23 +6,16 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
 
-    protected $_postFactory;
-
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
-        \Mindarc\Survey\Model\PostFactory $postFactory
+        \Magento\Framework\View\Result\PageFactory $pageFactory
     ) {
         $this->_pageFactory = $pageFactory;
-        $this->_postFactory = $postFactory;
         return parent::__construct($context);
     }
 
     public function execute()
     {
-        $resultPage = $this->_pageFactory->create();
-        $postCollection = $this->_postFactory->create()->getCollection();
-        $resultPage->getLayout()->getBlock('survey_index_index')->setData('posts', $postCollection);
-        return $resultPage;
+        return $this->_pageFactory->create();
     }
 }
